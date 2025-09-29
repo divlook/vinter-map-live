a6f06707fc1c55b17db1b97ce4f182262b963f34
uihyeon.cha <uihyeon.cha@heumlabs.io>
2025-09-29 11:10:42 +0900

    feat: add TailwindCSS and React

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
@@ -9,8 +11,13 @@ export default defineConfig(
     ignores: ['dist'],
   },
   {
-    files: ['**/*.{ts,js}'],
-    extends: [js.configs.recommended, tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       parser: tseslint.parser,
       ecmaVersion: 2020,
